6a03627351769046e6dce6123bf0f9e80b6efd1a
Андрей <andrej5122785@yandex.ru>
2022-10-30 15:45:40 +0300

    add model test

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,4 +1,7 @@
 from django.test import Client, TestCase
+from django.core.exceptions import ValidationError
+
+from .models import Item, Category, Tag
 
 
 class StaticURLTests(TestCase):
@@ -25,3 +28,127 @@ class StaticURLTests(TestCase):
     def test_catalog_page_zero(self):
         response = Client().get('/catalog/0')
         self.assertEqual(response.status_code, 404)
+
+
+class ItemModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.category = Category.objects.create(
+            is_published=True,
+            name='Test category',
+            slug='test-category-slug',
+            weight=300
+        )
+        cls.tag = Tag.objects.create(
+            is_published=True,
+            name='Test tag',
+            slug='test-tag-slug'
+        )
+
+    def test_unable_create_item(self):
+        item_count = Item.objects.count()
+
+        with self.assertRaises(ValidationError):
+            self.item = Item(
+                is_published=True,
+                name='Test item',
+                category=self.category,
+                text='no amazing'
+            )
+            self.item.full_clean()
+            self.item.save()
+            self.item.tags.add(self.tag)
+
+        self.assertEqual(Item.objects.count(), item_count)
+
+    def test_able_create_item(self):
+        item_count = Item.objects.count()
+
+        self.item = Item(
+            is_published=True,
+            name='Test item',
+            category=self.category,
+            text='превосходно'
+        )
+        self.item.full_clean()
+        self.item.save()
+        self.item.tags.add(self.tag)
+
+        self.assertEqual(Item.objects.count(), item_count + 1)
+
+
+class CategoryModelTest(TestCase):
+    def test_unable_create_category_weight_negative_number(self):
+        category_count = Category.objects.count()
+
+        with self.assertRaises(ValidationError):
+            self.category = Category(
+                is_published=True,
+                name='Test category',
+                slug='test-category-slug',
+                weight=-1
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(Category.objects.count(), category_count)
+
+    def test_unable_create_category_weight_to_big_number(self):
+        category_count = Category.objects.count()
+
+        with self.assertRaises(ValidationError):
+            self.category = Category(
+                is_published=True,
+                name='Test category',
+                slug='test-category-slug',
+                weight=400000
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(Category.objects.count(), category_count)
+
+    def test_unable_create_category_weight_not_number(self):
+        category_count = Category.objects.count()
+
+        with self.assertRaises(ValidationError):
+            self.category = Category(
+                is_published=True,
+                name='Test category',
+                slug='test-category-slug',
+                weight='ten'
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(Category.objects.count(), category_count)
+
+    def test_able_create_category(self):
+        category_count = Category.objects.count()
+
+        self.category = Category(
+            is_published=True,
+            name='Test category',
+            slug='test-category-slug',
+            weight=200
+        )
+        self.category.full_clean()
+        self.category.save()
+
+        self.assertEqual(Category.objects.count(), category_count + 1)
+
+
+class TagModelTest(TestCase):
+    def test_able_create_tag(self):
+        tag_count = Tag.objects.count()
+
+        self.tag = Tag(
+            is_published=True,
+            name='Test tag',
+            slug='test-tag-slug'
+        )
+        self.tag.full_clean()
+        self.tag.save()
+
+        self.assertEqual(Tag.objects.count(), tag_count + 1)
