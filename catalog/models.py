d7a39d656167046a6156b22f5dec00e4b1cbbc7b
Андрей <andrej5122785@yandex.ru>
2022-11-02 12:34:05 +0300

    add test for canonical name

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,6 @@
 import re
 
 from django.db import models
-from django.db.models.signals import pre_save
 from .validators import validate_weight, validate_amazing
 from core.models import MainInfo
 from django.core.exceptions import ValidationError
@@ -14,10 +13,11 @@ class Category(MainInfo):
     canonical_name = models.CharField('каноническое название', max_length=150, unique=True, editable=False)
 
     def clean(self):
-        can_name = re.sub(r'[^\s\w]', '', self.name.lower())
-        if Category.objects.filter(canonical_name=can_name).count() != 0 and \
-                Category.objects.filter(canonical_name=can_name).first().id != self.id:
+        self.canonical_name = re.sub(r'[^\s\w]', '', self.name.lower())
+        if (Category.objects.filter(canonical_name=self.canonical_name).count() != 0 and
+                Category.objects.filter(canonical_name=self.canonical_name).first().id != self.id):
             raise ValidationError('Данная категория уже есть')
+        super().clean()
 
     class Meta:
         verbose_name = 'категория'
@@ -29,10 +29,11 @@ class Tag(MainInfo):
     canonical_name = models.CharField('каноническое название', max_length=150, unique=True, editable=False)
 
     def clean(self):
-        can_name = re.sub(r'[^\s\w]', '', self.name.lower())
-        if Tag.objects.filter(canonical_name=can_name).count() != 0 and \
-                Tag.objects.filter(canonical_name=can_name).first().id != self.id:
+        self.canonical_name = re.sub(r'[^\s\w]', '', self.name.lower())
+        if (Tag.objects.filter(canonical_name=self.canonical_name).count() != 0 and
+                Tag.objects.filter(canonical_name=self.canonical_name).first().id != self.id):
             raise ValidationError('Данный тег уже есть')
+        super().clean()
 
     class Meta:
         verbose_name = 'тег'
@@ -51,17 +52,3 @@ class Item(MainInfo):
     class Meta:
         verbose_name = 'товар'
         verbose_name_plural = 'товары'
-
-
-def catalog_pre_save(sender, instance, *args, **kwargs):
-    instance.canonical_name = re.sub(r'[^\s\w]', '', instance.name.lower())
-
-
-pre_save.connect(catalog_pre_save, sender=Category)
-
-
-def tag_pre_save(sender, instance, *args, **kwargs):
-    instance.canonical_name = re.sub(r'[^\s\w]', '', instance.name.lower())
-
-
-pre_save.connect(catalog_pre_save, sender=Tag)
--- catalog/tests.py
+++ catalog/tests.py
@@ -113,9 +113,36 @@ class ItemModelTest(TestCase):
 
 
 class CategoryModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.category = Category.objects.create(
+            is_published=True,
+            name='Книга',
+            slug='test-category-slug',
+            weight=300
+        )
+        cls.category.full_clean()
+        cls.category.save()
+
     def tearDown(self) -> None:
         super().tearDown()
         Category.objects.all().delete()
+
+    def test_unable_create_category_already_exists(self):
+        category_count = Category.objects.count()
+
+        with self.assertRaises(ValidationError):
+            self.category = Category(
+                is_published=True,
+                name='КНиГа!!',
+                slug='another-test-category-slug',
+                weight=300
+            )
+            self.category.full_clean()
+            self.category.save()
+
+        self.assertEqual(Category.objects.count(), category_count)
 
     def test_unable_create_category_weight_negative_number(self):
         category_count = Category.objects.count()
@@ -139,7 +166,7 @@ class CategoryModelTest(TestCase):
             self.category = Category(
                 is_published=True,
                 name='Test category',
-                slug='test-category-slug',
+                slug='category-slug',
                 weight=400000
             )
             self.category.full_clean()
@@ -168,7 +195,7 @@ class CategoryModelTest(TestCase):
         self.category = Category(
             is_published=True,
             name='Test category',
-            slug='test-category-slug',
+            slug='category-slug-test',
             weight=200
         )
         self.category.full_clean()
@@ -178,9 +205,34 @@ class CategoryModelTest(TestCase):
 
 
 class TagModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.tag = Tag(
+            is_published=True,
+            name='Акция',
+            slug='tag-slug'
+        )
+        cls.tag.full_clean()
+        cls.tag.save()
+
     def tearDown(self) -> None:
         super().tearDown()
         Tag.objects.all().delete()
+
+    def test_unable_create_tag_already_exists(self):
+        tag_count = Tag.objects.count()
+
+        with self.assertRaises(ValidationError):
+            self.tag = Tag(
+                is_published=True,
+                name='АКЦИЯ!!!!!!!!!!!!!',
+                slug='test-slug'
+            )
+            self.tag.full_clean()
+            self.tag.save()
+
+        self.assertEqual(Tag.objects.count(), tag_count)
 
     def test_able_create_tag(self):
         tag_count = Tag.objects.count()
