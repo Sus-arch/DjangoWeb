e83a9ea45bec7465850189e6fe7869ab86328c7e
Андрей <andrej5122785@yandex.ru>
2022-11-27 02:19:44 +0300

    add redefine password form

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,33 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
         super(LoginForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
+
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(CustomPasswordChangeForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
+
+class CustomPasswordResetForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(CustomPasswordResetForm, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
+
+
+class CustomSetPasswordForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(CustomSetPasswordForm, self).__init__(*args, **kwargs)
         for field in self.visible_fields():
             field.field.widget.attrs['class'] = 'form-control'
--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,37 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views
 from django.urls import path
 
-from users.forms import LoginForm
+from users.forms import LoginForm, CustomPasswordChangeForm, CustomPasswordResetForm, CustomSetPasswordForm
 
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/',
-         LoginView.as_view(template_name='users/login.html',
-                           authentication_form=LoginForm),
+         views.LoginView.as_view(template_name='users/login.html',
+                                 authentication_form=LoginForm),
          name='login'),
     path('logout/',
-         LogoutView.as_view(template_name='users/logout.html'),
+         views.LogoutView.as_view(template_name='users/logout.html'),
          name='logout'),
+    path('password_change/',
+         views.PasswordChangeView.as_view(template_name='users/password_change.html',
+                                          form_class=CustomPasswordChangeForm),
+         name='password_change'),
+    path('password_change/done/',
+         views.PasswordChangeDoneView.as_view(template_name='users/password_changed.html')),
+    path('reset_password/',
+         views.PasswordResetView.as_view(template_name='users/reset_password.html',
+                                         form_class=CustomPasswordResetForm),
+         name='reset_password'),
+    path('password_reset/done/',
+         views.PasswordResetDoneView.as_view(template_name='users/reset_mail_send.html'),
+         name='reset_mail_send'),
+    path('reset/<uidb64>/<token>/',
+         views.PasswordResetConfirmView.as_view(template_name='users/reset_password_confirm.html',
+                                                form_class=CustomSetPasswordForm),
+         name='reset_password_confirm'),
+    path('reset/done/',
+         views.PasswordResetDoneView.as_view(template_name='users/reset_password_done.html'),
+         name='reset_password_done'),
 ]
