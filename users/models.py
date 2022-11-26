9d8fd4188db9517cd5fbe148db8eda197b0410aa
Андрей <andrej5122785@yandex.ru>
2022-11-26 23:50:33 +0300

    add birthday in admin panel

--- users/models.py
+++ users/models.py
@@ -4,7 +4,10 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birthday = models.DateTimeField('день рождения', blank=True, null=True)
+    birthday = models.DateField('день рождения', blank=True, null=True)
+
+    def __str__(self):
+        return f'День рождения пользователя {self.user}'
 
     class Meta:
         verbose_name = 'дополнительная информация'
